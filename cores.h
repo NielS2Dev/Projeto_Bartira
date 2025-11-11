f4d48c305bcdd9d4e6e12cd00627085fd146a738
Antoniel <antonielsilvaniels2oficial@gmail.com>
2025-11-11 05:32:53 +0000

    Removidos todos os arquivos do projeto

--- cores.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORES_H
-#define CORES_H
-
-// Declaração da função para mudar a cor do texto no console.
-// O corpo desta função (usando SetConsoleTextAttribute no Windows
-// ou sequências ANSI no Linux) deve estar em cores.c.
-void mudarCor(int cor);
-
-#endif
